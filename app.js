ae2b09c2c8834a879e1a0b09cf222fc14f39bb01
Aristides Vicente <72865211+DarkSun-dev@users.noreply.github.com>
2023-12-15 12:25:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 dotenv.config({ path: './config/enviropment.env' })
 const port = process.env.PORT || 8000;
-const host = process.env.HOST || '0.0.0.0'
 const express = require('express')
 const path = require('path')
 const app = express()
 
+require('./init/db.js')
+require('./init/middleware')(app)
+require('./init/router')(app)
 
-require('./init/db.js')(app, () => {
-  require('./init/middleware')(app);
-  require('./init/router')(app);
-  app.use(express.static(path.join(__dirname, './public')))
-  app.set("views", path.join(__dirname, "./public"));
-  app.set("view engine", "html");
-  app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')) })
-  app.listen(port, (error) => {
-    if (error) throw error;
-    console.log('Your app is listening on ' + port);
-  });
-});
+app.use(express.json({ limit: '10mb' }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+app.use(express.static(path.join(__dirname, './public')))
+app.set("views", path.join(__dirname, "./public"));
+app.set("view engine", "html");
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')) })
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log('Your app is listening on ' + port);
+})
+
